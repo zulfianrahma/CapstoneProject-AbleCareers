28a02054f3b938cc6592c4e23354f4a76a9a67f2
Zulfian Rahmadiansyah <zulfian2018@mail.ugm.ac.id>
2023-06-15 23:14:49 +0700

    melengkapi index.js, app.js, dan menambahkan file drawer-initiator.js

--- Front-End/src/scripts/views/app.js
+++ Front-End/src/scripts/views/app.js
@@ -1,3 +1,5 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+
 class App {
   constructor({ button, drawer, content }) {
     this._button = button;
@@ -7,7 +9,13 @@ class App {
     this._initialAppShell();
   }
 
-  _initialAppShell() {}
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+  }
 }
 
 export default App;

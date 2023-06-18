f6d0a7021dccca177068941bc96c5020fd193aaf
aappiinn <125990275+aappiinn@users.noreply.github.com>
2023-06-19 03:16:26 +0700

    Add files via upload

---
+++ About us/src/scripts/views/app.js
@@ -0,0 +1,13 @@
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {}
+}
+
+export default App;

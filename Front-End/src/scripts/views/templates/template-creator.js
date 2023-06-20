eb088e1898a0397e8db0ed22a4ac9f165bde2af0
Zulfian Rahmadiansyah <zulfian2018@mail.ugm.ac.id>
2023-06-20 23:19:11 +0700

    update css dan profile page

--- Front-End/src/scripts/views/templates/template-creator.js
+++ Front-End/src/scripts/views/templates/template-creator.js
@@ -222,34 +222,35 @@ const createTeamIdentityTemplate = () => `
 
 const createProfileTemplate = () => `
 <div class="profile-top">
-    <div class="profile-picture" id="profile-picture">
-      <input type="file" id="picture-input" class="file-input" accept="image/*">
-      <button id="upload-button" class="upload-button" style="display: none;">Upload Picture</button>
-    <div>
+<div class="profile-picture" id="profile-picture"></div>
+<div class="profile-button">
+<input type="file" id="picture-input" class="file-input" accept="image/*">
+<button id="upload-button" class="upload-button" style="display: none;">Upload Picture</button>
+</div>
 
-    <p>USER PROFILE</p>
-    
-    <div class="profile-info">
-      <label>Name</label>
-      <p class="profile-text" id="profile-name"></p>
-      <input type="text" id="name-input" class="editable-field" style="display: none;">
-      <hr>
+<p>USER PROFILE</p>
+</div>
+<div class="profile-info">
+  <label>Name</label>
+  <p class="profile-text" id="profile-name"></p>
+  <input type="text" id="name-input" class="editable-field" style="display: none;">
+  <hr>
 
-      <label>Address</label>
-      <p class="profile-text" id="profile-address"></p>
-      <input type="text" id="address-input" class="editable-field" style="display: none;">
-      <hr>
- 
-      <label id="bio-label">Biography</label>
-      <p class="profile-text" id="profile-bio"></p>
-      <textarea id="bio-input" class="editable-field" style="display: none;"></textarea>
-      <hr>
- 
-    
- 
-      <button id="edit-button" class="edit-button">Edit Profile</button>
-      <button id="save-button" class="save-button" style="display: none;">Save Changes</button>
-    </div>
+  <label>Address</label>
+  <p class="profile-text" id="profile-address"></p>
+  <input type="text" id="address-input" class="editable-field" style="display: none;">
+  <hr>
+
+  <label id="bio-label">Biography</label>
+  <p class="profile-text" id="profile-bio"></p>
+  <textarea id="bio-input" class="editable-field" style="display: none;"></textarea>
+  <hr>
+
+
+
+  <button id="edit-button" class="edit-button">Edit Profile</button>
+  <button id="save-button" class="save-button" style="display: none;">Save Changes</button>
+</div>
 `;
 
 const createHomeBlogTemplate = () => `
